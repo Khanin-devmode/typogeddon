361d785362efc83a8b80bd8d45f59ae102513c63
Khanin <khanin.dev@gmail.com>
2023-04-17 22:36:33 +0700

    move spaceship and remove when hit a line

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 
 class SpaceShooterGame extends FlameGame with PanDetector {
   late Player player;
+  late Line line;
 
   @override
   Future<void> onLoad() async {
@@ -18,34 +19,57 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 
     player = Player()
       ..sprite = playerSprite
-      ..position = size / 2
+      ..position = Vector2(size.y, size.x / 2)
       ..width = 100
       ..height = 100
-      ..anchor = Anchor.center;
-
+      ..anchor = Anchor.topCenter;
     add(player);
+
+    final line = Line()
+      ..position = Vector2(size.x / 2, 100)
+      ..width = size.x
+      ..height = 1
+      ..anchor = Anchor.center;
+    add(line);
   }
 
   @override
   void onPanUpdate(DragUpdateInfo info) {
     player.move(info.delta.game);
   }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    player.move(Vector2(0, -0.5));
+
+    // if (player.position.y == line.position.y) {
+    if (player.position.y == 100) {
+      remove(player);
+    }
+  }
 }
 
-// class Player extends PositionComponent {
-//   static final _paint = Paint()..color = Colors.white;
+class Line extends PositionComponent {
+  static final _paint = Paint()..color = Colors.white;
 
-//   @override
-//   void render(Canvas canvas) {
-//     canvas.drawRect(size.toRect(), _paint);
-//   }
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
 
-//   void move(Vector2 delta) {
-//     position.add(delta);
-//   }
-// }
+  // void move(Vector2 delta) {
+  //   position.add(delta);
+  // }
+}
 
 class Player extends SpriteComponent with HasGameRef<SpaceShooterGame> {
+  void move(Vector2 delta) {
+    position.add(delta);
+  }
+}
+
+class Word extends TextComponent {
   void move(Vector2 delta) {
     position.add(delta);
   }
