03b15f1e5a22153d73b542b8a776638cc1879fa9
Khanin <khanin.dev@gmail.com>
2023-04-18 22:17:01 +0700

    simple on remove and random position

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
@@ -10,6 +12,9 @@ void main() {
 class SpaceShooterGame extends FlameGame with PanDetector {
   late Player player;
   late Line line;
+  late Word word1;
+
+  final random = Random();
 
   @override
   Future<void> onLoad() async {
@@ -17,20 +22,27 @@ class SpaceShooterGame extends FlameGame with PanDetector {
 
     final playerSprite = await loadSprite('player-sprite.png');
 
+    print(size);
+
     player = Player()
       ..sprite = playerSprite
-      ..position = Vector2(size.y, size.x / 2)
+      ..position = Vector2(size.x / 2, size.y)
       ..width = 100
       ..height = 100
       ..anchor = Anchor.topCenter;
     add(player);
 
-    final line = Line()
+    line = Line()
       ..position = Vector2(size.x / 2, 100)
       ..width = size.x
       ..height = 1
       ..anchor = Anchor.center;
     add(line);
+
+    word1 = Word()
+      ..text = 'Lorem'
+      ..position = Vector2(size.x * random.nextDouble(), size.y);
+    add(word1);
   }
 
   @override
@@ -41,11 +53,27 @@ class SpaceShooterGame extends FlameGame with PanDetector {
   @override
   void update(double dt) {
     super.update(dt);
-    player.move(Vector2(0, -0.5));
 
-    // if (player.position.y == line.position.y) {
-    if (player.position.y == 100) {
+    player.move(Vector2(0, -2));
+    word1.move(Vector2(0, -2));
+    // print('${player.position.y} : ${line.position.y}');
+    if (player.position.y == line.position.y) {
       remove(player);
+    }
+    if (word1.position.y == line.position.y) {
+      remove(word1);
+    }
+
+    if (player.isRemoved) {
+      player.position.x = size.x * random.nextDouble();
+      player.position.y = size.y;
+      add(player);
+    }
+
+    if (word1.isRemoved) {
+      word1.position.x = size.x * random.nextDouble();
+      word1.position.y = size.y;
+      add(word1);
     }
   }
 }
